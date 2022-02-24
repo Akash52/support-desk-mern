dc4fbb778e40ce9ce823c4e6e70268aa295b2c5f
Akash52 <ac8572611@gmail.com>
2022-02-24 22:38:45 +0530

    Connect to the Database

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
