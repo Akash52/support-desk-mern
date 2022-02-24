3867284ffd05e698ffc6ed6bac9e1ee9a9864ea8
Akash52 <ac8572611@gmail.com>
2022-02-24 21:16:42 +0530

    Basic Epress Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(201).json({ message: 'Welcome to Support Desk!' });
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on port ${PORT} mode is ${process.env.NODE_ENV}`
+  );
+});
