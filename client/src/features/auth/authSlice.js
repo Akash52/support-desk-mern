8507d22ae0d1574cc67365a209d69626c67e091e
Akash52 <ac8572611@gmail.com>
2022-02-26 14:28:16 +0530

    Redux Setup & Auth Slice

---
+++ client/src/features/auth/authSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: null,
+  isError: false,
+  isLoading: false,
+  message: '',
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default authSlice.reducer;
