4e135bd24ad007ae8a36fc99d8dbe00f68d501f5
Akash52 <ac8572611@gmail.com>
2022-02-26 12:56:09 +0530

    Header & Initial Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Header from './components/Header';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+
+function Header() {
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <Link to='/'> Support Desk </Link>
+      </div>
+      <ul>
+        <li>
+          <Link to='/login'>
+            <FaSignInAlt />
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to='/register'>
+            <FaSignOutAlt />
+            Register
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
