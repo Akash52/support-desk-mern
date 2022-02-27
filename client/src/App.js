498e03959c003794f5bdd2cad3f99bbb448859f2
Akash52 <ac8572611@gmail.com>
2022-02-27 22:31:25 +0530

    Rote Guard

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Login from './pages/Login';
 import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import NewTicket from './pages/NewTicket';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
@@ -17,6 +19,9 @@ const App = () => {
             <Route path='/' element={<Home />} />
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
---
+++ client/src/pages/NewTicket.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewTicket = () => {
+  return <h1>NewTicket</h1>;
+};
+
+export default NewTicket;
